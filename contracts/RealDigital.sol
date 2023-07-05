bc2422dfc6c57e961d29ad60114697e56de03481
Gustavo <gustavo@obex.pw>
2023-07-05 09:13:13 -0300

    added pausable modifiers to "RealDigital"

--- contracts/RealDigital.sol
+++ contracts/RealDigital.sol
@@ -6,20 +6,17 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "./CBDCAccessControl.sol";
 
 /// @title RealDigital Contract
-/// @notice An ERC20 token contract with additional features such as freezing balances and access control.
+/// @notice An ERC20 token contract with additional features such as freezing balances, access control, and pausable functionality.
 contract RealDigital is ERC20, Pausable, CBDCAccessControl {
-    mapping(address => uint256) public frozenBalanceOf;  // Mapping to track frozen balances
+    mapping(address => uint256) public frozenBalanceOf; // Mapping to track frozen balances
 
-    event FrozenBalance(address wallet, uint256 amount);  // Event emitted when a balance is frozen
+    event FrozenBalance(address wallet, uint256 amount); // Event emitted when a balance is frozen
 
     modifier checkFrozenBalance(address from, uint256 amount) {
-        require(balanceOf(from) - frozenBalanceOf[from] >= amount, "RealDigital: insufficient frozen balance");
-        _;
-    }
-
-    /// @dev Modifier to restrict functions to authorized participants only.
-    modifier onlyParticipant() {
-        require(isAuthorizedParticipant(msg.sender), "RealDigital: caller is not an authorized participant");
+        require(
+            balanceOf(from) - frozenBalanceOf[from] >= amount,
+            "RealDigital: insufficient frozen balance"
+        );
         _;
     }
 
@@ -38,23 +35,31 @@ contract RealDigital is ERC20, Pausable, CBDCAccessControl {
     /// @dev Checks if an address is an authorized participant.
     /// @param participant The address to check.
     /// @return A boolean indicating if the address is an authorized participant.
-    function isAuthorizedParticipant(address participant) public view returns (bool) {
+    function isAuthorizedParticipant(
+        address participant
+    ) public view returns (bool) {
         return super.verifyAccount(participant);
     }
 
     /// @dev Mints new tokens and adds them to the specified address.
     /// @param to The address to mint tokens to.
     /// @param amount The amount of tokens to mint.
-    function mint(address to, uint256 amount) public {
-        require(hasRole(MINTER_ROLE, msg.sender), "RealDigital: must have minter role to mint");
+    function mint(address to, uint256 amount) public whenNotPaused {
+        require(
+            hasRole(MINTER_ROLE, msg.sender),
+            "RealDigital: must have minter role to mint"
+        );
         _mint(to, amount);
     }
 
     /// @dev Burns tokens from a specified address.
     /// @param from The address to burn tokens from.
     /// @param amount The amount of tokens to burn.
-    function burnFrom(address from, uint256 amount) public {
-        require(hasRole(BURNER_ROLE, msg.sender), "RealDigital: must have burner role to burn");
+    function burnFrom(address from, uint256 amount) public whenNotPaused {
+        require(
+            hasRole(BURNER_ROLE, msg.sender),
+            "RealDigital: must have burner role to burn"
+        );
         _burn(from, amount);
     }
 
@@ -66,7 +71,7 @@ contract RealDigital is ERC20, Pausable, CBDCAccessControl {
         address from,
         address to,
         uint256 amount
-    ) internal virtual override checkFrozenBalance(from, amount) {
+    ) internal virtual override checkFrozenBalance(from, amount) whenNotPaused {
         super._beforeTokenTransfer(from, to, amount);
     }
 }
